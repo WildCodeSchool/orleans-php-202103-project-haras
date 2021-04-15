935ed77b517591801362ba2b493892e9dafeae21
bouboumael <mael-chariault_student2021@wilder.school>
2021-04-15 14:58:57 +0200

    create formulary validation

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,39 @@
 
 namespace App\Controller;
 
+use App\Services\THP\Formulary;
+
 class AdminController extends AbstractController
 {
-    public function course(): string
+    public function course(string $success = null): string
     {
-        return $this->twig->render('Admin/course.html.twig');
+        $errors = [];
+        $formData = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $formData = $_POST;
+            $errors = (new Formulary($formData, [
+                'name' => [],
+                'time-start' => [],
+                'duration' => [],
+                'teacher' => [],
+                'capacity' => [],
+            ]))->validateForm();
+
+            $formData = array_map('trim', $formData);
+
+            if (empty($errors)) {
+                header('location: /Admin/course/success');
+            }
+        }
+
+        if ($success !== null) {
+            $success = 'Cours enregistré avec succès!';
+        }
+
+        return $this->twig->render('Admin/course.html.twig', [
+            'errors' => $errors,
+            'formulaire' => $formData,
+            'success' => $success,
+        ]);
     }
 }
