a72bc6cb76295a71f17a0503d3f3b871f02ce87b
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-04-21 16:15:33 +0200

    changes done

--- src/Controller/StageController.php
+++ src/Controller/StageController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 class StageController extends AbstractController
 {
-    public function stage()
+    public function stage(): string
     {
         return $this->twig->render('User/stage.html.twig');
     }
