a1d23ec5b76602b168a0ce897b447cd0066b9864
bouboumael <mael-chariault_student2021@wilder.school>
2021-04-21 15:08:44 +0200

    render change

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,6 +21,6 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->twig->render('Home/index.html.twig');
+        return $this->twig->render('User/welcome.html.twig');
     }
 }
