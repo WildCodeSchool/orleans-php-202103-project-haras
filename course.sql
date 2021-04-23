d81cbc66529eb0ee2b7b5c4b618ae48b913bdf7e
bouboumael <mael-chariault_student2021@wilder.school>
2021-04-24 00:20:24 +0200

    course.sql with create and insert for test

---
+++ course.sql
@@ -0,0 +1,17 @@
+-- create table course
+CREATE TABLE course (
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `day` VARCHAR(8) NOT NULL,
+  `time` VARCHAR(5) NOT NULL,
+  `duration` INT NOT NULL,
+  `capacity` INT NOT NULL
+);
+--insert many fake courses for test
+INSERT INTO
+  course (`name`, `day`, `time`, `duration`, `capacity`)
+VALUES
+  ('Baby Poney', 'lundi', '09:00', 60, 20),
+  ('Baby Poney', 'mardi', '09:00', 60, 20),
+  ('Poney Débutant', 'jeudi', '10:00', 90, 20),
+  ('Poney Confirmé', 'mercredi', '15:00', 120, 20);
