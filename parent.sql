292188bb6d15e91641037b2a51c4dfcfa637b855
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-04-27 10:46:12 +0200

    Ajouts bases de données Parents et pupils

---
+++ parent.sql
@@ -0,0 +1,7 @@
+CREATE TABLE parent (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `firstname` VARCHAR(255) NOT NULL,
+    `lastname` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `phone_number` INT NOT NULL,
+);
