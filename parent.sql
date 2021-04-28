0cad172c1a4c3dde9b62cb7dbaa2349519b6d6a2
wilder <NEL45>
2021-04-28 15:50:50 +0200

    Good rectifications tables

--- parent.sql
+++ parent.sql
@@ -1,7 +1,7 @@
-CREATE TABLE parent-stages (
+CREATE TABLE parent (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `firstname` VARCHAR(255) NOT NULL,
     `lastname` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
-    `phone_number` INT NOT NULL,
+    `phone_number` INT NOT NULL
 );
