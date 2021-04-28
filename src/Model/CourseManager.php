e9d62d2ca4ae565d445e06d69f6159473c1f329e
bouboumael <mael-chariault_student2021@wilder.school>
2021-04-28 15:34:36 +0200

    editing

--- src/Model/CourseManager.php
+++ src/Model/CourseManager.php
@@ -22,4 +22,18 @@ class CourseManager extends AbstractManager
         $statement->bindValue('capacity', $item['capacity'], \PDO::PARAM_INT);
         $statement->execute();
     }
+
+    public function update(array $item): void
+    {
+        $query = "UPDATE " . self::TABLE .
+            " SET name=:name, day=:day, time=:time, duration=:duration, capacity=:capacity WHERE id=:id";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('name', $item['name'], \PDO::PARAM_STR);
+        $statement->bindValue('day', $item['day'], \PDO::PARAM_STR);
+        $statement->bindValue('time', $item['time'], \PDO::PARAM_STR);
+        $statement->bindValue('duration', $item['duration'], \PDO::PARAM_INT);
+        $statement->bindValue('capacity', $item['capacity'], \PDO::PARAM_INT);
+        $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }
