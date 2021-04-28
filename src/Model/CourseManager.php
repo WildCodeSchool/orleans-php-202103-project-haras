1cb8c403a167acfa6bc551ffeb9f90b9b71e5cff
bouboumael <mael-chariault_student2021@wilder.school>
2021-04-28 16:23:04 +0200

    change sql pdo type

--- src/Model/CourseManager.php
+++ src/Model/CourseManager.php
@@ -16,7 +16,7 @@ class CourseManager extends AbstractManager
             VALUES (:name, :day, :time, :duration, :capacity)";
         $statement = $this->pdo->prepare($query);
         $statement->bindValue('name', $item['name'], \PDO::PARAM_STR);
-        $statement->bindValue('day', $item['day'], \PDO::PARAM_STR);
+        $statement->bindValue('day', $item['day'], \PDO::PARAM_INT);
         $statement->bindValue('time', $item['time'], \PDO::PARAM_STR);
         $statement->bindValue('duration', $item['duration'], \PDO::PARAM_INT);
         $statement->bindValue('capacity', $item['capacity'], \PDO::PARAM_INT);
@@ -29,7 +29,7 @@ class CourseManager extends AbstractManager
             " SET name=:name, day=:day, time=:time, duration=:duration, capacity=:capacity WHERE id=:id";
         $statement = $this->pdo->prepare($query);
         $statement->bindValue('name', $item['name'], \PDO::PARAM_STR);
-        $statement->bindValue('day', $item['day'], \PDO::PARAM_STR);
+        $statement->bindValue('day', $item['day'], \PDO::PARAM_INT);
         $statement->bindValue('time', $item['time'], \PDO::PARAM_STR);
         $statement->bindValue('duration', $item['duration'], \PDO::PARAM_INT);
         $statement->bindValue('capacity', $item['capacity'], \PDO::PARAM_INT);
