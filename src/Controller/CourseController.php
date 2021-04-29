a3c0f985cd7b236c220165df37c183ea1da68067
bouboumael <mael-chariault_student2021@wilder.school>
2021-04-29 15:02:34 +0200

    refactor gestion picture in twig

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -3,15 +3,16 @@
 namespace App\Controller;
 
 use App\Model\CourseManager;
-use DateTime;
 
 class CourseController extends AbstractController
 {
     public function course()
     {
+        $imagesCarousel = ['faceharas.jpg', 'poneycarousel.jpg', 'prairieharas.jpg'];
         $courses = (new CourseManager())->selectDistinctName();
         return $this->twig->render('User/course.html.twig', [
             'courses' => $courses,
+            'images' => $imagesCarousel,
         ]);
     }
 }
