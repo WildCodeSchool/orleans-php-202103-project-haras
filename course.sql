35fa5eb156d2d274f664be17d50a5d222ed0a8d5
wilder <NEL45>
2021-04-30 09:33:52 +0200

    Ajout test table course sql

--- course.sql
+++ course.sql
@@ -1,16 +1,14 @@
--- create table course
 CREATE TABLE course (
-  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `day` INT NOT NULL,
-  `time` TIME NOT NULL,
-  `duration` INT NOT NULL,
-  `capacity` INT NOT NULL,
-  `age` INT NOT NULL
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  day INT NOT NULL,
+  time TIME NOT NULL,
+  duration INT NOT NULL,
+  capacity INT NOT NULL,
+  age INT NOT NULL
 );
---insert many fake courses for test
 INSERT INTO
-  course (`name`, `day`, `time`, `duration`, `capacity`, `age`)
+  course (name, day, time, duration, capacity, age)
 VALUES
   ('Baby Poney', 1, '09:00', 60, 20, 2),
   ('Poney Débutant', 2, '09:00', 60, 20, 3),
