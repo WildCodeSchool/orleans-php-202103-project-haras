b50f71c9aed00cf0e111c52f7a70882158762190
bouboumael <mael-chariault_student2021@wilder.school>
2021-05-03 00:55:07 +0200

    create calendar/css/service

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use App\Model\CourseManager;
+use App\Service\Sort;
 
 class CourseController extends AbstractController
 {
     public function course()
     {
+        $courseManager = new CourseManager();
         $imagesCarousel = ['faceharas.jpg', 'poneycarousel.jpg', 'prairieharas.jpg'];
-        $courses = (new CourseManager())->selectDistinctName();
+        $courses = $courseManager->selectDistinctName();
+        $coursesByDay = (new Sort())->sortingCoursesByDay($courseManager->selectAll('time'));
         return $this->twig->render('User/course.html.twig', [
             'courses' => $courses,
             'images' => $imagesCarousel,
+            'coursesByDay' => $coursesByDay,
         ]);
     }
 }
