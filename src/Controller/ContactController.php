b827e935311b25c558b912e825a30f9dc0633dd3
Wowlfy <maxime.giraudeau@hotmail.fr>
2021-05-04 21:29:49 +0200

    fixed form and secure

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,10 +2,45 @@
 
 namespace App\Controller;
 
+
 class ContactController extends AbstractController
 {
     public function contact(): string
     {
-        return $this->twig->render('User/contact.html.twig');
+        $message = '';
+        $data = [];
+        $errors = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = array_map('trim', $_POST);
+
+            if (empty($data['firstname'])) {
+                $errors[] = 'Le prénom est obligatoire';
+            }
+            if (empty($data['lastname'])) {
+                $errors[] = 'Le nom est obligatoire';
+            }
+            if (empty($data['email'])) {
+                $errors[] = 'L\'email est obligatoire';
+            }
+            if (empty($data['phone'])) {
+                $errors[] = 'Le numero de téléphone est obligatoire';
+            }
+            if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors[] = 'Mauvais format d\'email';
+            }
+            if (empty($data['message'])) {
+                $errors[] = 'Un message est obligatoire';
+            }
+            if (empty($errors)) {
+                $message = 'Votre message a bien été envoyé';
+                $data = null;
+            }
+        }
+        return $this->twig->render('User/contact.html.twig',[
+            'success' => $message,
+            'data' => $data,
+            'errors' => $errors,
+        ]);
     }
 }
