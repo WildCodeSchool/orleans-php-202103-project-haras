e2652fb2ad2c48d4c80e9649604f0f3e178b6ea7
Wowlfy <maxime.giraudeau@hotmail.fr>
2021-05-05 11:43:45 +0200

    fixed contact grey block html

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 class ContactController extends AbstractController
 {
+    private const LENGHT = 255;
     public function contact(): string
     {
         $message = '';
@@ -12,6 +13,7 @@ class ContactController extends AbstractController
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $data = array_map('trim', $_POST);
+
 
             if (empty($data['firstname'])) {
                 $errors[] = 'Le prénom est obligatoire';
@@ -25,12 +27,11 @@ class ContactController extends AbstractController
             if (empty($data['phone'])) {
                 $errors[] = 'Le numero de téléphone est obligatoire';
             }
-            if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-                $errors[] = 'Mauvais format d\'email';
-            }
+
             if (empty($data['message'])) {
                 $errors[] = 'Un message est obligatoire';
             }
+            $errors = array_merge($errors, $this->validate($data));
             if (empty($errors)) {
                 $message = 'Votre message a bien été envoyé';
                 $data = null;
@@ -42,5 +43,22 @@ class ContactController extends AbstractController
             'data' => $data,
             'errors' => $errors,
         ]);
+    }
+    private function validate(array $data): array
+    {
+        $errors = [];
+        if (strlen($data['message']) > self::LENGHT) {
+            $errors[] = 'Le message doit faire moins de 255 caractères';
+        }
+        if (strlen($data['phone']) > self::LENGHT) {
+            $errors[] = 'Le numero de telephone doit faire moins de 12 caractères';
+        }
+        if (strlen($data['lastname']) > self::LENGHT) {
+            $errors[] = 'Le nom doit faire moins de 255 caractères';
+        }
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Mauvais format d\'email';
+        }
+        return $errors;
     }
 }
